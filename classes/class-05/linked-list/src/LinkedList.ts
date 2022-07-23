0a2beaa9eb6f5ef544615b9e585fde6b4274105c
David Souther <davidsouther+github@gmail.com>
2022-07-23 10:07:06 -0700

    TS Solution, for TAs

--- classes/class-05/linked-list/src/LinkedList.ts
+++ classes/class-05/linked-list/src/LinkedList.ts
@@ -1,3 +1,34 @@
-import { Collection } from "./Collection";
+import { Collection, display } from "./Collection";
 
-export class LinkedList<T> implements Collection<T> {}
+export class LinkedList<T> implements Collection<T> {
+  private head?: LinkedListNode<T>;
+
+  insert(value: T) {
+    this.head = { value, next: this.head };
+  }
+
+  includes(value: T): boolean {
+    let node = this.head;
+    while (node) {
+      if (node.value === value) return true;
+      node = node.next;
+    }
+    return false;
+  }
+
+  toString(): string {
+    const str = [];
+    let node = this.head;
+    while (node) {
+      str.push(`{ ${display(node.value)} }`);
+      node = node.next;
+    }
+    str.push("NULL");
+    return str.join(" -> ");
+  }
+}
+
+interface LinkedListNode<T> {
+  value: T;
+  next?: LinkedListNode<T>;
+}
