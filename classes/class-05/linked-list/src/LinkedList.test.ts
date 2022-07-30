b8342e3652cc572ccc9a07861c5d19e31c946b3a
David Souther <davidsouther+github@gmail.com>
2022-07-30 10:14:01 -0700

    Clean up class 5

--- classes/class-05/linked-list/src/LinkedList.test.ts
+++
@@ -1,9 +0,0 @@
-import { Collection } from "./Collection";
-import { LinkedList } from "./LinkedList";
-
-describe("linked list", () => {
-  it("runs my custom test", () => {
-    const list: Collection<string> = new LinkedList<string>();
-    expect(list).toBeDefined();
-  });
-});
