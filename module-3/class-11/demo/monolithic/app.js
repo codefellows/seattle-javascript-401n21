c6861035a0a5430b945082d50fba04aa1f991611
David Souther <davidsouther+github@gmail.com>
2022-08-13 11:26:15 -0700

    After lecture

--- module-3/class-11/demo/monolithic/app.js
+++ module-3/class-11/demo/monolithic/app.js
@@ -2,35 +2,32 @@ const EventEmitter = require("events");
 const chance = require("chance");
 const Chance = new chance();
 
+// The weatherStation is an EventEmitter
+const weatherStation = new EventEmitter();
+
 // Weather?
 // change - {temperature, cloudCover, preciptation}
 
 // Every hour - our weather station measures tmp, clouiness, rain
 
 function makeRecording() {
+  // A utility that uses Chance to make a payload for the weather change event
   return {
     temperature: Chance.floating({ min: 0, max: 100 }),
     precipitation: Chance.floating({ min: 0, max: 5 }),
-    clouds: Chance.pick(["Clear", "Partial", "Overcast"]),
+    clouds: Chance.pickone(["Clear", "Partial", "Overcast"]),
   };
 }
-
-const weatherStation = new EventEmitter();
 
 weatherStation.addListener("change", (recording) =>
   console.log("Listener 1", recording)
 );
 
-// weatherStation.emit("change", { id: 1, ...makeRecording() });
-
-// weatherStation.addListener("change", (recording) =>
-//   console.log("Listener 2", recording)
-// );
-
 weatherStation.emit("change", { id: 2, ...makeRecording() });
 
-weatherStation.addListener("change", (recording) => {
+const derek = (recording) => {
   console.log("Derek");
+
   // If it's raining, put on a jacket
   if (recording.precipitation > 0.5) {
     console.log("Derek puts on a jacket");
@@ -39,32 +36,44 @@ weatherStation.addListener("change", (recording) => {
   if (recording.clouds === "Clear") {
     console.log("Derek puts on a hat");
   }
-});
+};
 
-weatherStation.addListener("change", (recording) => {
+weatherStation.addListener("change", derek);
+
+const danny = (recording) => {
   console.log("Danny");
   // If it's snowing, go snowboarding
   if (recording.temperature < 32 && recording.precipitation > 0) {
     console.log("Danny goes snowboarding");
   }
-});
+};
+
+weatherStation.addListener("change", danny);
 
 weatherStation.emit("change", { id: 3, ...makeRecording() });
 
-weatherStation.addListener("change", (recording) => {
+const hugo = (recording) => {
   console.log("Hugo");
   if (recording.precipitation >= 2) {
     console.log("Hugo SCUBA Dives");
   }
-});
 
-setInterval(() => {
-  weatherStation.emit("change", makeRecording());
-}, 2000);
+  if (recording.temperature <= 40) {
+    weatherStation.removeListener("change", hugo);
+  }
+};
 
-weatherStation.addListener("change", (recording) => {
+weatherStation.addListener("change", hugo);
+
+const tony = (recording) => {
   console.log("Tony");
   if (recording.temperature >= 70 && recording.precipitation < 0.5) {
     console.log("Tony goes for a bike ride");
   }
-});
+};
+
+weatherStation.addListener("change", tony);
+
+setInterval(() => {
+  weatherStation.emit("change", makeRecording());
+}, 2000);
