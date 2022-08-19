d370ea20a36a18e71afad815680faaf13b1fe967
David Souther <davidsouther+github@gmail.com>
2022-08-18 20:02:38 -0700

    Linked List solution

--- module-3/class-12/challenge/src/assignment.test.ts
+++ module-3/class-12/challenge/src/assignment.test.ts
@@ -10,6 +10,8 @@ describe("Animal Shelter", () => {
 
     expect(shelter.dequeue("cat")).toBe("Pippin");
     expect(shelter.dequeue("cat")).toBe("Greylien");
+    expect(shelter.dequeue("cat")).toBe(undefined);
+    expect(shelter.dequeue("cat")).toBe(undefined);
   });
 
   it("gets the next dog", () => {
@@ -20,6 +22,8 @@ describe("Animal Shelter", () => {
     shelter.enqueue("Greylien", "cat");
 
     expect(shelter.dequeue("dog")).toBe("Oliver");
+    expect(shelter.dequeue("dog")).toBe(undefined);
+    expect(shelter.dequeue("dog")).toBe(undefined);
   });
 
   it("gets the next friend", () => {
@@ -31,5 +35,25 @@ describe("Animal Shelter", () => {
 
     expect(shelter.dequeue()).toBe("Pippin");
     expect(shelter.dequeue()).toBe("Oliver");
+    expect(shelter.dequeue()).toBe("Greylien");
+    expect(shelter.dequeue()).toBe(undefined);
+  });
+
+  it("recovers after empty", () => {
+    const shelter = new Shelter();
+
+    shelter.enqueue("Pippin", "cat");
+    shelter.enqueue("Oliver", "dog");
+    shelter.enqueue("Greylien", "cat");
+
+    expect(shelter.dequeue()).toBe("Pippin");
+    expect(shelter.dequeue()).toBe("Oliver");
+    expect(shelter.dequeue()).toBe("Greylien");
+    expect(shelter.dequeue()).toBe(undefined);
+
+    shelter.enqueue("Puffinus", "cat");
+    expect(shelter.dequeue("dog")).toBe(undefined);
+    expect(shelter.dequeue()).toBe("Puffinus");
+    expect(shelter.dequeue()).toBe(undefined);
   });
 });
--- module-3/class-12/challenge/src/shelter.ts
+++ module-3/class-12/challenge/src/shelter.ts
@@ -1,19 +1,58 @@
 interface Animal {
   name: string;
   type: "cat" | "dog";
+  prev?: Animal;
+  next?: Animal;
 }
 
 export class Shelter {
+  newestFriend: Animal | undefined;
+  oldestFriend: Animal | undefined;
+
+  enqueue(name: string, type: "cat" | "dog"): void {
+    this.newestFriend = { name, type, next: this.newestFriend };
+    if (!this.oldestFriend) {
+      this.oldestFriend = this.newestFriend;
+    } else {
+      this.newestFriend.next!.prev = this.newestFriend;
+    }
+  }
+
+  dequeue(type?: "cat" | "dog"): string | undefined {
+    let friend = this.oldestFriend;
+    if (type !== undefined) {
+      while (friend && friend?.type !== type) {
+        friend = friend?.prev;
+      }
+    }
+    if (friend === undefined) {
+      return undefined;
+    }
+    if (friend?.prev) {
+      friend.prev.next = friend.next;
+    } else {
+      this.newestFriend = friend.next;
+    }
+    if (friend?.next) {
+      friend.next.prev = friend.prev;
+    } else {
+      this.oldestFriend = friend.prev;
+    }
+    return friend.name;
+  }
+}
+
+export class ShelterArray {
   friends: Animal[] = [];
 
   enqueue(name: string, type: "cat" | "dog"): void {
     this.friends.push({ name, type });
   }
 
-  dequeue(type?: "cat" | "dog"): string {
+  dequeue(type?: "cat" | "dog"): string | undefined {
     if (type === undefined) {
       const friend = this.friends.shift();
-      return friend?.name ?? "";
+      return friend?.name;
     } else {
       const idx = this.friends.findIndex((f) => f.type === type);
       const friend = this.friends[idx];
