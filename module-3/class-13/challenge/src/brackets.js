4aafbe2fb2ee34814eb828ab95b57174a79239a9
David Souther <davidsouther+github@gmail.com>
2022-08-22 18:50:36 -0700

    Reset the challenge for lecture

--- module-3/class-13/challenge/src/brackets.js
+++ module-3/class-13/challenge/src/brackets.js
@@ -1,33 +1,5 @@
 function balancedBrackets(str) {
-  // The stack of expected closing braces, in order
-  const stack = [];
-
-  for (let i = 0; i < str.length; i++) {
-    let c = str[i];
-
-    if (c === stack[stack.length - 1]) {
-      // c is an expected closing character
-      stack.pop();
-      continue;
-    }
-
-    if (c === "]" || c === "}" || c === ")") {
-      // c is an unexpected closing character, so we are unbalanced.
-      return false;
-    }
-
-    if (c === "[") {
-      stack.push("]");
-    } else if (c === "{") {
-      stack.push("}");
-    } else if (c === "(") {
-      stack.push(")");
-    }
-    // Pushed the appropriate close characters for the opening characters
-  }
-
-  // Made it through the entire string. It's balanced if the stack is empty!
-  return stack.length === 0;
+  return false;
 }
 
 module.exports = balancedBrackets;
