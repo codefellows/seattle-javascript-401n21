cf3a835e8a54934d827073aa9d6de4bef7ee3242
David Souther <davidsouther+github@gmail.com>
2022-08-27 10:51:19 -0700

    Class 15 starter

---
+++ module-3/class-15/src/assignment.btree.test.js
@@ -0,0 +1,25 @@
+const { BinaryTree, Node } = require("./binary_tree.js");
+
+describe("Binary Tree", () => {
+  const tree = new BinaryTree(
+    new Node(
+      1,
+      new Node(7, new Node(2), new Node(6, new Node(5), new Node(11))),
+      new Node(9),
+      new Node(9),
+      new Node(5)
+    )
+  );
+
+  it("does an pre-order traversal (root, left, right)", () => {
+    expect(tree.inOrder()).toEqual([1, 7, 2, 6, 5, 11, 9, 9, 5]);
+  });
+
+  it("does an post-order traversal (left, right, root)", () => {
+    expect(tree.inOrder()).toEqual([2, 3, 11, 6, 7, 5, 9, 9, 1]);
+  });
+
+  it("does an in-order traversal (left, root, right)", () => {
+    expect(tree.inOrder()).toEqual([2, 7, 5, 6, 11, 1, 9, 9, 5]);
+  });
+});
---
+++ module-3/class-15/src/binary_tree.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value, left, right) {}
+}
+
+class BinaryTree {
+  constructor(root) {
+    this.root = root;
+  }
+  preOrder() {}
+  inOrder() {}
+  postOrder() {}
+}
+
+module.exports = { Node, BinaryTree };
