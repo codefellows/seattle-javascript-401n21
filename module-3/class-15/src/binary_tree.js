b0023a5dce21b536c7b2f7b31edcecac0991f2f2
David Souther <davidsouther+github@gmail.com>
2022-08-27 14:26:06 -0700

    Class

--- module-3/class-15/src/binary_tree.js
+++ module-3/class-15/src/binary_tree.js
@@ -1,5 +1,57 @@
+function preOrder(root) {
+  // Root, Left, Right
+  // Return a single array
+  let traversal = [];
+  traversal.push(root.value); // Root
+
+  if (root.left) {
+    // Left
+    let leftTraversal = preOrder(root.left);
+    traversal = traversal.concat(leftTraversal);
+  }
+
+  // Right
+  if (root.right) {
+    let rightTraversal = preOrder(root.right);
+    traversal = traversal.concat(rightTraversal);
+  }
+
+  return traversal;
+}
+
+const preOrderOneLine = (root) =>
+  root ? [root.value, ...preOrder(root.left), ...preOrder(root.right)] : [];
+
 class Node {
-  constructor(value, left, right) {}
+  constructor(value, left, right) {
+    this.value = value;
+    this.left = left; // A Node
+    this.right = right; // A Node
+  }
+
+  preOrder() {
+    // Root, Left, Right
+    // Return a single array
+    let traversal = [];
+    traversal.push(this.value); // Root
+
+    if (this.left) {
+      // Left
+      let leftTraversal = this.left.preOrder();
+      traversal = traversal.concat(leftTraversal);
+    }
+
+    // Right
+    if (this.right) {
+      let rightTraversal = this.right.preOrder();
+      traversal = traversal.concat(rightTraversal);
+    }
+
+    return traversal;
+  }
+
+  inOrder() {}
+  postOrder() {}
 }
 
 class BinaryTree {
@@ -7,7 +59,10 @@ class BinaryTree {
     this.root = root;
   }
 
-  preOrder() {}
+  preOrder() {
+    // return this.root.preOrder();
+    return preOrder(this.root);
+  }
   inOrder() {}
   postOrder() {}
 }
