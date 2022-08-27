0dbd936501529f82b5dc5303a1b2e405d82ea229
David Souther <davidsouther+github@gmail.com>
2022-08-27 14:26:06 -0700

    Class

--- module-3/class-15/src/assignment.bst.test.js
+++ module-3/class-15/src/assignment.bst.test.js
@@ -1,6 +1,6 @@
 const { BinarySearchTree } = require("./binary_search_tree.js");
 
-describe("Binary Search Tree", () => {
+describe.skip("Binary Search Tree", () => {
   const tree = new BinarySearchTree();
   for (const i of [2, 3, 11, 6, 9, 7, 5, 1]) {
     tree.add(i);
