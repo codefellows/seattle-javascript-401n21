ff73b6a1348400e136a68b4b539fe1aa3d49e4e1
David Souther <davidsouther@gmail.com>
2022-10-11 19:38:11 -0700

    Dispatch / reducer

--- module-5/class-29/sam-and-cookies/src/App.tsx
+++ module-5/class-29/sam-and-cookies/src/App.tsx
@@ -1,7 +1,63 @@
+import React, { useCallback, useReducer, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+interface AppState {
+  method: "GET" | "POST" | "PUT";
+  url: string;
+  body: object | undefined;
+  response: object | undefined;
+}
+
+// Set of actions that are isolated & modify this state
+export const changeMethod = (
+  state: AppState,
+  method: "GET" | "POST" | "PUT"
+) => {
+  if (method === "GET") {
+    state.body = undefined;
+  }
+  return {
+    ...state,
+    method,
+  };
+};
+
+export const setBodyIfNotGet = (state: AppState, body: object) => {
+  // This action will set state.body, only IF state.method is not "GET"
+  if (state.method !== "GET") {
+    state.body = body;
+  }
+  return state;
+};
+
+export const dispatchAction = (state: AppState, action: {action: string, body: unknown}) {
+  switch (action.action) {
+    case 'method':
+      return changeMethod(state, action.body as 'GET'|'POST'|'PUT');
+    case 'body':
+      return setBodyIfNotGet(state, action.body as object);
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(dispatchAction, {
+    method: "GET",
+    url: "http://localhost:3000",
+    body: undefined,
+    response: undefined
+  });
+
+  const updateBodyIfNotGet = (body: object) => {
+      dispatch({action: 'body', body: {}})
+    };
+
+  const updateMethod = (method: "GET"|"POST"|"PUT") => {
+    dispatch({action: "method", body: method});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
