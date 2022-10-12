3892280c3a6580d70497800b67cd23550ee9ed79
David Souther <davidsouther@gmail.com>
2022-10-11 19:38:11 -0700

    Dispatch / reducer

--- module-5/class-29/sam-and-cookies/src/App.test.tsx
+++ module-5/class-29/sam-and-cookies/src/App.test.tsx
@@ -7,3 +7,25 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("updates method state", async () => {
+  render(<App />);
+  // NOthing I can do to get method or setMethod
+
+  await fireEvent.select(screen.getByTestId("method-selector"), "POST");
+
+  expect(screen.getByText("POST")).toBeInTheDocument();
+});
+
+test("updateBodyIfNotGet", async () => {
+  render(<App />);
+  await fireEvent.select(screen.getByTestId("method-selector"), "GET");
+  await fireEvent.change(screen.getByTestId("body"), JSON.stringify("{}"));
+  // etc
+});
+
+test("setBodyIfNotGet", () => {
+  const state = { method: "GET" };
+  const state2 = setBodyIfNotGet(state, {});
+  expect(state2.body).toBeUndefined();
+});
