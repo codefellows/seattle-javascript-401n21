650ac677c621bec2b3725a9f803c02c40f0a77f1
John Cokos <highlander.44@gmail.com>
2022-10-29 14:32:38 -0700

    Auth0 Wiring

--- module-6/demo/auth-app/src/auth/Auth.js
+++ module-6/demo/auth-app/src/auth/Auth.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {When} from 'react-if';
+
+import {LoginContext} from './context.js';
 
 class Login extends React.Component {
 
+  static contextType = LoginContext;
+
   render() {
+
+    const isLoggedIn = this.context.loggedIn;
+    const canDo = this.props.capability ? this.context.can(this.props.capability) : true;
+    const okToRender = isLoggedIn && canDo;
+
     // TODO: See if we're logged in AND (maybe) have the capability
     //       If so, we render the children
     //       If not, we render null
     return (
-      <>
+      <When condition={okToRender}>
         {this.props.children}
-      </>
+      </When>
     )
   }
 
